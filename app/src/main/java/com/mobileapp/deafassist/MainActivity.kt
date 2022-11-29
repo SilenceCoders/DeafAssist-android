aaffa8fe49612b2a1c86e041b8bfda03b5528985
Sepehr Madani <43821543+1ssepehr@users.noreply.github.com>
2022-11-28 22:09:48 -0500

    Add NavController control

--- app/src/main/java/com/mobileapp/deafassist/MainActivity.kt
+++ app/src/main/java/com/mobileapp/deafassist/MainActivity.kt
@@ -2,6 +2,8 @@ package com.mobileapp.deafassist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.mobileapp.deafassist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
