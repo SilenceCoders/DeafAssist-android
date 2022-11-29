b790f1fd97791b76f2e4d2dd6644831f3b4c3f1a
Sepehr Madani <43821543+1ssepehr@users.noreply.github.com>
2022-11-28 22:07:27 -0500

    Add Barebone UI

---
+++ app/src/main/java/com/mobileapp/deafassist/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.mobileapp.deafassist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mobileapp.deafassist.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    // This property is only valid between onCreateView and onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
--- app/src/main/java/com/mobileapp/deafassist/MainActivity.kt
+++ app/src/main/java/com/mobileapp/deafassist/MainActivity.kt
@@ -2,10 +2,13 @@ package com.mobileapp.deafassist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mobileapp.deafassist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
