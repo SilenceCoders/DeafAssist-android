f82e15eae10e2df4e4e557875a8206c73b1252d7
Sepehr Madani <43821543+1ssepehr@users.noreply.github.com>
2022-12-09 07:27:12 -0500

    Implement Room Database (BUGGED)

--- app/src/main/java/com/mobileapp/deafassist/favoriteList/FavoriteListDialogFragment.kt
+++ app/src/main/java/com/mobileapp/deafassist/favoriteList/FavoriteListDialogFragment.kt
@@ -5,20 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.mobileapp.deafassist.data.FavoriteEntity
+import com.mobileapp.deafassist.data.FavoritesApplication
 import com.mobileapp.deafassist.databinding.FragmentItemListDialogListDialogBinding
 import com.mobileapp.deafassist.databinding.FragmentItemListDialogListDialogItemBinding
-import com.mobileapp.deafassist.favoriteList.FavoriteViewModel
 
 
 // TODO: Customize parameter argument names
 const val ARG_ITEM_COUNT = "item_count"
 
 /**
- *
  * A fragment that shows a list of items as a modal bottom sheet.
  *
  * You can show this modal bottom sheet from your activity like this:
@@ -29,32 +32,34 @@ const val ARG_ITEM_COUNT = "item_count"
 class FavoriteListDialogFragment : BottomSheetDialogFragment() {
 
     private var _binding: FragmentItemListDialogListDialogBinding? = null
-
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
+    // ViewModel object
+    val favoriteViewModel: FavoriteViewModel by  activityViewModels {
+        FavoriteViewModelFactory((activity?.application as FavoritesApplication).repository)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentItemListDialogListDialogBinding.inflate(inflater, container, false)
+
+        // Add vertical divider space between elements
         binding.list.addItemDecoration(
             DividerItemDecoration(
                 context,
                 DividerItemDecoration.VERTICAL
             )
         )
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.list.layoutManager = LinearLayoutManager(context)
         binding.list.adapter = FavoriteAdapter(
-            listOf(
-                FavoriteViewModel("Sepehr"),
-                FavoriteViewModel("Smart"),
-                FavoriteViewModel("Student")
-            )
+            favoriteViewModel.allFavorites
         )
     }
 
@@ -68,7 +73,7 @@ class FavoriteListDialogFragment : BottomSheetDialogFragment() {
     }
 
     // Adapter Class for RecyclerView
-    private inner class FavoriteAdapter internal constructor(private val mList: List<FavoriteViewModel>) :
+    private inner class FavoriteAdapter internal constructor(private val mList: LiveData<List<FavoriteEntity>>) :
         RecyclerView.Adapter<ViewHolder>() {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -81,11 +86,20 @@ class FavoriteListDialogFragment : BottomSheetDialogFragment() {
 
         // binds the list items to a view
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            holder.text.text = mList[position].text
+            holder.text.text = favoriteViewModel.allFavorites.value?.get(position)?.text
+
+            // TODO: Figure out what to do with allFavorites.
+            // allFavorites is coming in as a LiveData<List<FavoriteEntity>>
+//            favoriteViewModel.allFavorites.observe(this, Observer { favorites ->
+//                // Update the cached copy of the words in the adapter.
+//                favorites.let { binding.list.adapter.submitList(it) }
+//            })
         }
 
-        override fun getItemCount() = mList.size
-    }
+        override fun getItemCount(): Int {
+            return if (favoriteViewModel.allFavorites.value == null) 0 else
+                favoriteViewModel.allFavorites.value!!.size
+        }    }
 
     override fun onDestroyView() {
         super.onDestroyView()
